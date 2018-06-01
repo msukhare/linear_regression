ec594613badd6f9e40d7f2f8e65eb4bebe1e525a
sukharev <msukhare@student.42.fr>
2018-06-01 17:18:02 +0200

    push dataset + algo termine

--- reg_lineair.py
+++ reg_lineair.py
@@ -6,7 +6,7 @@
 #    By: msukhare <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/05/30 13:44:17 by msukhare          #+#    #+#              #
-#    Updated: 2018/05/31 17:11:13 by msukhare         ###   ########.fr        #
+#    Updated: 2018/06/01 17:04:53 by msukhare         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -51,50 +51,46 @@ def hypo(X_scale, i, thetas, col_X):
     j = 0
     to_ret = 0
     while (j < col_X):
-        to_ret += thetas[j][0] * X_scale
+        to_ret += thetas[j][0] * X_scale[i][j]
         j += 1
     return (to_ret)
 
-def squar(hypo, y):
-    return (hypo * hypo - (2 * hypo * y) + (y * y))
-
-def cost_fct(thetas, X_scale, Y_scale, row_X, col_X, alpha):
+def cost_fct(thetas, X_scale, Y, row_X, col_X):
     i = 0
     somme = 0
     while (i < row_X):
-        somme += squar(hypo(X_sclale, i, thetas, col_X), Y_scale[i][0])
+        somme += (hypo(X_scale, i, thetas, col_X) - Y[i][0])**2
         i += 1
-    return ()
+    return ((1 / (2 * row_X)) * somme)
 
+def get_sum(X_scale, Y, row_X, col_X, pl_the, thetas):
+    i = 0
+    somme = 0
+    while (i < row_X):
+        somme += (hypo(X_scale, i, thetas, col_X) - Y[i][0]) * X_scale[i][pl_the]
+        i += 1
+    return (somme)
 
+def guardient_descent(thetas, tmp_thetas, X_scale, Y, row_X, col_X):
+    i = 0
+    alpha = 0.1
+    while (i < col_X):
+        tmp_thetas[i][0] = thetas[i][0] - (alpha * (1 / row_X) * get_sum(X_scale, Y, row_X, col_X, i, thetas))
+        i += 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def make_predi(thetas, tmp_thetas, X_scale, Y_scale, row_X, col_X):
-    alpha = 0.01
-    
+def make_predi(thetas, tmp_thetas, X_scale, Y, row_X, col_X):
+    res_bef = 0
+    while (res_bef != cost_fct(thetas, X_scale, Y, row_X, col_X)):
+        res_bef = cost_fct(thetas, X_scale, Y, row_X, col_X)
+        guardient_descent(thetas, tmp_thetas, X_scale, Y, row_X, col_X)
+        i = 0
+        while (i < col_X):
+            thetas[i][0] = tmp_thetas[i][0]
+            i += 1
 
 def main():
     data, X, Y, thetas, tmp_thetas = read_file()
     X_scale = np.array(X, dtype=float)
-    Y_scale = np.array(Y, dtype=float)
     row_X = X.shape[0]
     col_X = X.shape[1]
     j = 1
@@ -103,13 +99,14 @@ def main():
         max_X = get_min_max(X, j, row_X, 0)
         scale_feat(X_scale, j, row_X, min_X, max_X)
         j += 1
-    min_Y = get_min_max(Y, 0, row_X, 1)
-    max_Y = get_min_max(Y, 0, row_X, 0)
-    scale_feat(Y_scale, 0, row_X, min_Y, max_Y)
-    make_predi(thetas, tmp_thetas, X_scale, Y_scale, row_X, col_X)
-  #  print(min_Y, max_Y, Y_scale)
-
-
+    make_predi(thetas, tmp_thetas, X_scale, Y, row_X, col_X)
+    j = 1
+    while (j < col_X):
+        min_X = get_min_max(X, j, row_X, 1)
+        max_X = get_min_max(X, j, row_X, 0)
+        thetas[j][0] = thetas[j][0] / (max_X - min_X)
+        j += 1
+    print(thetas)
 
 if __name__ == "__main__":
     main()
